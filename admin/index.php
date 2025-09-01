b9e68cfb77f8e7f1a2e13d538f86aeee950c88a0
nibbo <131214845+NibboNi@users.noreply.github.com>
2025-08-31 23:45:45 -0600

    add table styles

--- admin/index.php
+++ admin/index.php
@@ -37,22 +37,51 @@ require "../includes/header.php"
   <?php if (empty($articles)): ?>
     <p>No articles found.</p>
   <?php else: ?>
-    <table>
-      <thead>
-        <th>title</th>
-      </thead>
-      <tbody>
-        <?php foreach ($articles as $article): ?>
-          <tr>
-            <td>
-              <a href="article.php?id=<?= $article["id"]; ?>">
-                <?= htmlspecialchars($article["title"]); ?>
-              </a>
-            </td>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
+    <div class="table-container">
+      <table class="table">
+        <thead>
+          <th>title</th>
+          <th>published at</th>
+          <th>last edited</th>
+          <th colspan="2">actions</th>
+        </thead>
+        <tbody>
+          <?php foreach ($articles as $article): ?>
+            <tr>
+              <td>
+                <a href="/admin/article.php?id=<?= $article["id"]; ?>">
+                  <?= htmlspecialchars($article["title"]); ?>
+                </a>
+              </td>
+              <td>
+                <time datetime="<?= date_format(new DateTime($article["published_at"]), "Y-m-d H:i:s") ?>">
+                  <?= htmlspecialchars($article["published_at"]); ?>
+                </time>
+              </td>
+              <td>
+                <?php if (isset($article["updated_at"])): ?>
+                  <time datetime="<?= date_format(new DateTime($article["updated_at"]), "Y-m-d H:i:s") ?>">
+                    <?= htmlspecialchars($article["updated_at"]); ?>
+                  </time>
+                <?php else: ?>
+                  <p>Not yet edited</p>
+                <?php endif; ?>
+              </td>
+              <td>
+                <a href="/admin/edit-article.php?id=<?= $article["id"]; ?>" class="form-action">
+                  Edit
+                </a>
+              </td>
+              <td>
+                <a href="/admin/delete-article.php?id=<?= $article["id"]; ?>" class="form-action form-action--delete">
+                  Delete
+                </a>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
   <?php endif; ?>
 
 </main>
