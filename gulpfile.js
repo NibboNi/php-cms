916a79d82f5acecca1754b12fc0e43223e7626c7
nibbo <131214845+NibboNi@users.noreply.github.com>
2025-09-02 21:42:44 -0600

    add gulp setup
    Added gulp and related dependencies to develop a better dev environment to work with sass and minified js

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+import * as fs from "fs";
+import path from "path";
+
+import { dest, series, src, watch } from "gulp";
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
+import terser from "gulp-terser";
+import replace from "gulp-replace";
+
+import sharp from "sharp";
+import browserSync from "browser-sync";
+
+const sass = gulpSass(dartSass);
+const bs = browserSync.create();
+
+function buildStyles() {
+  return src("src/scss/**/*.scss", { sourcemaps: true })
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(dest("dist/assets/css", { sourcemaps: "." }))
+    .pipe(bs.stream());
+}
+
+function minifyJS() {
+  return src("src/js/**/*.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest("dist/assets/js", { sourcemaps: "." }))
+    .pipe(bs.stream());
+}
+
+async function convertImages() {
+  const imgDir = "uploads";
+  const files = fs.readdirSync(imgDir);
+
+  const images = files.filter(file =>
+    [".jpg", ".jpeg", ".png"].includes(path.extname(file).toLowerCase())
+  );
+
+  await Promise.all(
+    images.map(async file => {
+      const { name } = path.parse(file);
+      const inputPath = path.join(imgDir, file);
+      const outputWebp = path.join(imgDir, `${name}.webp`);
+      const outputAvif = path.join(imgDir, `${name}.avif`);
+
+      try {
+        await sharp(inputPath).webp().toFile(outputWebp);
+        await sharp(inputPath).avif().toFile(outputAvif);
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  );
+}
+
+function bsStart(done) {
+  bs.init({
+    proxy: "http://localhost:8080",
+    open: true,
+  });
+
+  done();
+}
+
+function bsReload(done) {
+  bs.reload();
+
+  done();
+}
+
+function moveToDist() {
+  return src("includes/**").pipe(dest("dist/includes/"));
+}
+
+function linkAssets() {
+  return src(["dist/includes/header.php", "dist/includes/footer.php"])
+    .pipe(replace("/dist/assets/js/app.js", "/assets/js/app.js"))
+    .pipe(replace("/dist/assets/css/index.css", "/assets/css/index.css"))
+    .pipe(dest("dist/includes/"));
+}
+
+function dev() {
+  watch("src/scss/**/*.scss", buildStyles);
+  watch("src/js/**/*.js", minifyJS);
+  watch("**/*.php", bsReload);
+}
+
+export { convertImages };
+export const build = series(moveToDist, linkAssets);
+export default series(buildStyles, minifyJS, bsStart, dev);
---
+++ src/js/app.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+  /*! img_support.js v1.0.0 | github.com/MoisesKMS/img_support */
+  (function (document) {
+    var formats = {
+      avif: "image/avif",
+      webp: "image/webp",
+    };
+
+    function setHTMLClass(format, supported) {
+      if (supported) {
+        document.documentElement.classList.add(format);
+      } else {
+        document.documentElement.classList.add("not" + format);
+      }
+    }
+
+    function checkImageFormat(format) {
+      var image = new Image();
+      image.onload = image.onerror = function () {
+        setHTMLClass(format, image.height === 2);
+      };
+      image.src = "data:" + formats[format] + ";base64," + getImageData(format);
+    }
+
+    function getImageData(format) {
+      return {
+        avif: "AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=",
+        webp: "UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA",
+      }[format];
+    }
+
+    for (var format in formats) {
+      checkImageFormat(format);
+    }
+  })(
+    (window.sandboxApi &&
+      window.sandboxApi.parentWindow &&
+      window.sandboxApi.parentWindow.document) ||
+      document
+  );
+
+  toggleNav();
+
+  function toggleNav() {
+    const navBtn = document.querySelector("#navBtn");
+
+    if (!navBtn) return;
+
+    const navMenu = document.querySelector("#navMenu");
+
+    navBtn.addEventListener("click", () => {
+      navMenu.classList.toggle("open");
+    });
+  }
+});
