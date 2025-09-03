810d573eec52d080d41fa7b7270421d85e06a9b0
nibbo <131214845+NibboNi@users.noreply.github.com>
2025-09-03 13:16:46 -0600

    Update assets

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,8 @@
+import formatSupport from "./imageSupport.js";
+import toggleNav from "./toggleNav.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-  /*! img_support.js v1.0.0 | github.com/MoisesKMS/img_support */
-  (function (document) {
-    var formats = {
-      avif: "image/avif",
-      webp: "image/webp",
-    };
-
-    function setHTMLClass(format, supported) {
-      if (supported) {
-        document.documentElement.classList.add(format);
-      } else {
-        document.documentElement.classList.add("not" + format);
-      }
-    }
-
-    function checkImageFormat(format) {
-      var image = new Image();
-      image.onload = image.onerror = function () {
-        setHTMLClass(format, image.height === 2);
-      };
-      image.src = "data:" + formats[format] + ";base64," + getImageData(format);
-    }
-
-    function getImageData(format) {
-      return {
-        avif: "AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=",
-        webp: "UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA",
-      }[format];
-    }
-
-    for (var format in formats) {
-      checkImageFormat(format);
-    }
-  })(
+  formatSupport(
     (window.sandboxApi &&
       window.sandboxApi.parentWindow &&
       window.sandboxApi.parentWindow.document) ||
@@ -40,16 +10,4 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 
   toggleNav();
-
-  function toggleNav() {
-    const navBtn = document.querySelector("#navBtn");
-
-    if (!navBtn) return;
-
-    const navMenu = document.querySelector("#navMenu");
-
-    navBtn.addEventListener("click", () => {
-      navMenu.classList.toggle("open");
-    });
-  }
 });
---
+++ src/js/toggleNav.js
@@ -0,0 +1,13 @@
+function toggleNav() {
+  const navBtn = document.querySelector("#navBtn");
+
+  if (!navBtn) return;
+
+  const navMenu = document.querySelector("#navMenu");
+
+  navBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+  });
+}
+
+export default toggleNav;
