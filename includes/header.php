f7567ce38cf32015c2605c02aadf772cd2265b4f
nibbo <131214845+NibboNi@users.noreply.github.com>
2025-09-04 17:32:29 -0600

    update login/out button styles

--- includes/header.php
+++ includes/header.php
@@ -39,10 +39,12 @@
           </a>
         <?php endif; ?>
 
-        <a href="<?= Auth::isLoggedIn() ? "/logout.php" : "/login.php"; ?>" class="main-header__link main-header__link--btn mt-auto">
+        <a href="<?= Auth::isLoggedIn() ? "/logout.php" : "/login.php"; ?>" class="main-header__link main-header__link--btn <?= Auth::isLoggedIn() ? "mt-auto" : ""; ?>">
+
           <span>
             <?= Auth::isLoggedIn() ? "logout" : "login"; ?>
           </span>
+
           <?php if (Auth::isLoggedIn()): ?>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <title>logout</title>
@@ -54,6 +56,7 @@
               <path d="M11 7L9.6 8.4L12.2 11H2V13H12.2L9.6 15.6L11 17L16 12L11 7M20 19H12V21H20C21.1 21 22 20.1 22 19V5C22 3.9 21.1 3 20 3H12V5H20V19Z" />
             </svg>
           <?php endif; ?>
+
         </a>
 
       </nav>
