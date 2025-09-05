92bcefe36819ee532392d87006429a9ca6437c71
nibbo <131214845+NibboNi@users.noreply.github.com>
2025-09-04 22:10:35 -0600

    update assets linking

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
   <time datetime="2025-08-22" class="main-footer__date">2025/08/22</time>
 </footer>
 
-<script src="/dist/assets/js/app.js" type="module"></script>
+<script src="/src/assets/js/app.js" type="module"></script>
 <script async="" src="http://dev.cms:3000/browser-sync/browser-sync-client.js"></script>
 </body>
 
