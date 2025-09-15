e2b1d88c9c145f452ba951efb42d8fbfe677f60d
nibbo <131214845+NibboNi@users.noreply.github.com>
2025-09-15 15:26:58 -0600

    feat: update form validator to validate emails

--- src/js/validateForm.js
+++ src/js/validateForm.js
@@ -19,11 +19,16 @@ function validateForm(form) {
       if (maxLength > 0 && validateLength(input, maxLength)) {
         return false;
       }
+      if (input.type === "email") {
+        if (!validateEmail(input.value)) {
+          return false;
+        }
+      }
 
       return true;
     };
 
-    const showWarnings = () => {
+    const showWarnings = (onlyOnBlur = false) => {
       removeWarning(inputContainer.querySelector(".form-warning"));
 
       if (validateEmpty(input)) {
@@ -37,14 +42,25 @@ function validateForm(form) {
           `Warning: a ${fieldName} can't have more than ${maxLength} characters.`
         );
       }
+
+      if (onlyOnBlur) {
+        if (input.type === "email") {
+          if (!validateEmail(input.value)) {
+            invalidWarning(
+              inputContainer,
+              `Warining: ${input.value} is not a valid email.`
+            );
+          }
+        }
+      }
     };
 
-    const handleValidation = (show = false) => {
+    const handleValidation = (show = false, onlyOnBlur = false) => {
       const isValid = checkValidity();
       errors[input.name] = isValid;
 
       if (show && !isValid) {
-        showWarnings();
+        showWarnings(onlyOnBlur);
       } else if (isValid) {
         removeWarning(inputContainer.querySelector(".form-warning"));
       }
@@ -53,7 +69,7 @@ function validateForm(form) {
     };
 
     input.addEventListener("input", () => handleValidation(true));
-    input.addEventListener("blur", () => handleValidation(true));
+    input.addEventListener("blur", () => handleValidation(true, true));
 
     handleValidation();
   });
@@ -61,6 +77,14 @@ function validateForm(form) {
 
 function toggleSubmitBtn(errors, submitBtn) {
   submitBtn.disabled = Object.values(errors).includes(false);
+}
+
+function validateEmail(email) {
+  return String(email)
+    .toLocaleLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 }
 
 function validateEmpty(input) {
