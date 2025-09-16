c2e7b02a22211d36b9f633e069be6d4c785d8863
nibbo <131214845+NibboNi@users.noreply.github.com>
2025-09-15 19:53:12 -0600

    feat: add config.php init

--- includes/init.php
+++ includes/init.php
@@ -13,3 +13,33 @@ spl_autoload_register(function ($class) {
 });
 
 session_start();
+
+require dirname(__DIR__) . "/config.php";
+
+function errorHandler($level, $message, $file, $line)
+{
+  throw new ErrorException($message, 0, $level, $file, $line);
+}
+
+function exceptionHandler($exception)
+{
+  echo "<div class='local-debug'>";
+  echo "<h1>An error ocurred</h1>";
+
+  if (SHOW_ERROR_DETAIL) {
+    echo "<p>Uncaught exception: '" . get_class($exception) . "'</p>";
+    echo "<p>'" . $exception->getMessage() . "'</p>";
+    echo "<p>Stack trace: <pre>" . $exception->getTraceAsString() . "</pre></p>";
+    echo "<p>In file '" . $exception->getFile() . "' on line " . $exception->getLine() . "</p>";
+  } else {
+    echo "<p>Please try again latar.</p>";
+  }
+
+  echo "</div>";
+
+  exit();
+}
+
+
+set_error_handler("errorHandler");
+set_exception_handler("exceptionHandler");
